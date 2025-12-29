e22820ae90f0cd4a85eb99c5d697b1e047b38977
shiva-palakurthy <shivapalakurthy119@gmail.com>
2025-12-29 13:15:25 +0530

    some topics relayed to the maths and created a mad libs story , basic list programming

---
+++ mad_libs_game.py
@@ -0,0 +1,9 @@
+adjective = input("enter a adjective :")
+verb = input("enter a verb :")
+animal = input("enter a animal name: ")
+past_tense= input("enter a past tense :")
+
+print("That day became the most " + adjective + " moment of my life.")
+print("I grabbed my favorite backpack and decided to " + verb  + " outside.")
+print("At the park, I met a funny" + animal + " staring at me.")
+print("Suddenly, it "+past_tense +" and caused everyone to laugh.")
