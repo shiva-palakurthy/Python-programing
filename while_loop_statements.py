ffc2076319773fae3a88fddebb03e7c2a50e5f23
shiva-palakurthy <shivapalakurthy119@gmail.com>
2026-01-05 14:56:47 +0530

    examples of while loop

--- while_loop_statements.py
+++ while_loop_statements.py
@@ -12,3 +12,21 @@ guess = ""
 while guess != "qqq":
     guess = input("Enter a guess: ")
 print("your guess is correct")
+
+sl_no = 0
+while sl_no <=5:
+    print("hello rockline")
+    sl_no += 1
+print("thank you")
+
+I = 1
+sl_no = 0
+age = 18
+while sl_no <=5 and  I <=5 and age !=22:
+    print(I, "shiva" ,age)
+    I = I + 1
+    sl_no += 1
+    age += 1
+
+print("thank you")
+
